0b84277aec210e24b4244ae7facd6d04943a6c40
Sharminaktermetu <sharminacc1@gmail.com>
2023-06-09 23:12:02 +0600

    single tab server

--- index.js
+++ index.js
@@ -34,7 +34,12 @@ async function run() {
       const result =await cursor.toArray()
       res.send(result)
   })
-
+  app.get('/tabs/:id',async(req,res)=>{
+    const id =req.params.id;
+    const query ={_id:new ObjectId(id)}
+    const result =await categoryCollection.findOne(query);
+    res.send(result)
+  })
  
     // toy collection
     app.get('/toy',async(req,res)=>{ 
